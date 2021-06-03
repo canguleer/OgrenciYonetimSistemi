01c46bf9730b0e33679dfb486e93354601c535e7
CAN GÜLER <l5815046@std.yildiz.edu.tr>
2021-06-03 23:46:00 +0300

    öğretmen ve öğrenci controlller created..

---
+++ OgrenciYonetimSistemi/Controllers/OgrenciTanimlamaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OgrenciYonetimSistemi.Controllers
+{
+    public class OgrenciTanimlamaController : BaseController
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OgrenciYonetimSistemi/Controllers/OgretmenTanimlamaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OgrenciYonetimSistemi.Controllers
+{
+    public class OgretmenTanimlamaController : BaseController
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
